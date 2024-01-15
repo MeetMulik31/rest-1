82bb024c187809e70c1c0632e5d51af3e48d9544
Meet <116673001+MeetMulik31@users.noreply.github.com>
2024-01-15 21:25:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 1337;
 
+app.use(express.json());
+
+app.get('/',(request,response)=>{
+	response.sendStatus(200);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
